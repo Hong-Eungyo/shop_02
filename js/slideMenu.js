2d9ae25c2d4d049772fc8d9a7031aecbfcefcc73
Hong-Eungyo <vjvlaos75@naver.com>
2024-04-04 14:27:43 +0900

    20240404
    태그 수정, 슬라이드 메뉴 간격 조정

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -1,17 +1,24 @@
-const menuSlide = document.querySelector('.slide-menu-list');
-
+const menuSlide = document.querySelector(".slide-menu-list");
 
 // clone
 const clone = menuSlide.cloneNode(true);
 
-
 // Clone 추가
-document.querySelector('.slide-menu-box').appendChild(clone);
+document.querySelector(".slide-menu-box").appendChild(clone);
 
 // 원본, 복제본 위치 지정
-menuSlide.style.left = '0px';
-clone.style.left = document.querySelector('.slide-menu-list').offsetWidth + "px";
+menuSlide.style.left = "0px";
+clone.style.left =
+  document.querySelector(".slide-menu-list").offsetWidth + "px";
 
 //클래스 할당
-menuSlide.classList.add('original');
-clone.classList.add('clone');
+menuSlide.classList.add("original");
+clone.classList.add("clone");
+
+//사이즈 변경 시 새로고침
+window.addEventListener("resize", function () {
+  const innerWidth = window.innerWidth;
+  if (innerWidth < 600) {
+    location.reload();
+  }
+});
